6d9a69d172738d99bbdf746d404138fe600893d5
fujita <fujita1110@eos.ocn.ne.jp>
2023-09-02 14:19:37 +0900

    systemContent追加、初期化ボタン追加、shiftEnter追加

--- my-tkinter.py
+++ my-tkinter.py
@@ -2,51 +2,56 @@ import os
 import tkinter as tk
 import openai
 
-message_all = []
-
-def function_name(event=None):
-    
-    # 環境変数からapikeyを取得
-    openai.api_key = os.environ.get('YOUR_OPENAI_API_KEY')
-    send_message = text_area_request.get("1.0", tk.END)
-    message_all.append({"role":"user","content":send_message});
-
-    response = openai.ChatCompletion.create(
-            model = "gpt-3.5-turbo",
-            max_tokens = 500,
-            messages = message_all
-    )
-
-    response_message = response.choices[0]["message"]["content"].strip()
-    message_all.append({"role":"assistant","content":response_message})
+def ini_history(event=None):
+    message_all = [{"role":"system","content":"性格：子供,振る舞い：語尾に「だよ」をつける"}]
+    text_area_request.delete("1.0", tk.END)
+    text_area_response.delete("1.0", tk.END)
     print(message_all)
-    text_area_response.insert(tk.END, response_message + "\n")
 
-# Tkinterウィンドウを作成
+def send_request(event=None):
+    
+    if not event or (event.keysym == "Return" and not event.state & 0x1):
+
+        openai.api_key = os.environ.get('YOUR_OPENAI_API_KEY')
+        send_message = text_area_request.get("1.0", tk.END)
+        message_all.append({"role":"user","content":send_message})
+
+        response = openai.ChatCompletion.create(
+                model = "gpt-3.5-turbo",
+                max_tokens = 500,
+                messages = message_all
+        )
+
+        response_message = response.choices[0]["message"]["content"].strip()
+        message_all.append({"role":"assistant","content":response_message})
+        print(message_all)
+        text_area_response.insert(tk.END, response_message + "\n")
+
+message_all = [{"role":"system","content":"性格：子供,振る舞い：語尾に「だよ」をつける"}]
+
 window = tk.Tk()
 window.title("your window name")
 
-# 送信ボタンを作成
-button = tk.Button(window, text="メッセージを送信", command=function_name)
-button.pack(padx=20, pady=10)
+top_frame = tk.Frame(window)
+top_frame.pack()
 
-# 送信用テキストエリアのラベルを作成
-label = tk.Label(window, text="あなたのメッセージ")
-label.pack(padx=20, pady=10)
+button_send = tk.Button(top_frame, text="メッセージを送信", command=send_request)
+button_reset = tk.Button(top_frame, text="履歴を消す", command=ini_history)
+button_send.pack(padx=20, pady=10, side=tk.LEFT)
+button_reset.pack(padx=20, pady=10, side=tk.LEFT)
 
-# 送信用テキストエリアを作成
+label_request = tk.Label(window, text="あなたのメッセージ")
+label_request.pack(padx=20, pady=10)
+
 text_area_request = tk.Text(window)
 text_area_request.pack(padx=20, pady=10)
 
-# ラベルを作成
-label = tk.Label(window, text="GPTのメッセージ")
-label.pack(padx=20, pady=10)
+label_response = tk.Label(window, text="GPTのメッセージ")
+label_response.pack(padx=20, pady=10)
 
 text_area_response = tk.Text(window)
 text_area_response.pack(padx=20, pady=10)
 
-# Enterキーが押されたときに関数を実行するためのバインディングを追加
-text_area_request.bind("<Return>", function_name)
+text_area_request.bind("<Return>", send_request)
 
-# ウィンドウを表示
 window.mainloop()
